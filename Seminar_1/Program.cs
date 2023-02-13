2a0742d59ed1f18c35ca41a680aa696837bdbd8d
SpirinGennady <spiringena@yandex.ru>
2023-02-13 20:39:19 +0300

    First seminar added

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// int num = -12;
+// double bigNum = 12.13;
+// bool isEven = true;
+// string word = "flower";
+// char simbol = '#'
+// int num1 = -12;
+// int num2 = 6;
+
+// Console.WriteLine("my numbers are " + (num1 + num2) + " it is good!");
+// Console.WriteLine("My numbers are " + num1 + " and " + num2 + " it is good!");
+// Console.WriteLine($"My numbers are {num1} and {num2} it is good!");
+
+// Console.Write("Input an integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("your number is " + num);
+
+// Seminar_1
+
+// Task_1. Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quat2 = num2 * num2;
+
+if(num1 == quat2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+
+// Task_2. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = - num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+
+
+
