a6f405e12f5ac2d0302eae12f46fb0a6a7130384
SpirinGennady <spiringena@yandex.ru>
2023-02-15 13:46:59 +0300

    added again

---
+++ HomeWork/Program.cs
@@ -0,0 +1,47 @@
+﻿// HomeWork_1
+
+// Task_2. Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+//     Console.WriteLine($"the first number {num1} is greater than the second {num2}");
+// else
+//     Console.WriteLine($"the first number {num1} is less than the second {num2}");
+
+
+
+
+// Task_4. Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+
+// Task_6. Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+
+
+// Task_8. Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
