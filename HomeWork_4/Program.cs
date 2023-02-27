3d270a7d59b2ec8319447aebfb440fbe1920c677
SpirinGennady <spiringena@yandex.ru>
2023-02-27 13:23:28 +0300

    HomeWork_4 added

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -5,11 +5,30 @@
 
 // 2, 4 -> 16
 
-// int Extent(int a, int b)
+
+
+// void Construction(int a, int b)
 // {
-//     int current = 0;
-//     for()
+//     int result = 1;
+//     if (b <= 0)
+//     {
+//         Console.WriteLine("Error: The degree entered is not natural!");
+//     }
+//     else
+//     {
+//         for (int i = 1; i <= b; i++)
+//             result = result * a;
+//         Console.WriteLine($"The number {a} to the degree of {b} is {result}");
+//     }
 // }
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input the natural degree: ");
+// int pow = Convert.ToInt32(Console.ReadLine());
+// Construction(num, pow);
+
+
 
 
 // Задача 27: 
@@ -21,23 +40,23 @@
 
 // 9012 -> 12
 
-int SumUnit(int num)
-{
-    int sum = 0;
-    // if( num == o) sum = 1;
-    while(num != 0)
-    {
-        num /= 10;
-        sum++; 
-    }
-    return sum;
-}
+// int SumUnit(int num)
+// {
+//     int sum = 0;
+//    while (num > 0)
+// {
+//     int digit = num % 10;
+//     num = num / 10;
+//     sum = sum + digit;
+// }
+//     return sum;
+// }
 
-Console.Write(" Input a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write(" Input a number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-int result = SumUnit(number);
-Console.WriteLine($"accepts a number {number} as input and outputs the sum of digits {result}");
+// int result = SumUnit(number);
+// Console.WriteLine($"accepts a number {number} as input and outputs the sum of digits {result}");
 
 
 
@@ -47,3 +66,34 @@ Console.WriteLine($"accepts a number {number} as input and outputs the sum of di
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         Console.Write($"Input the value in the cell {i + 1}: ");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == 0) Console.Write("[ ");
+//         Console.Write(array[i]);
+//         if (i >= 0 && i != array.Length - 1) Console.Write(", ");
+//         else Console.Write(" ]");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.Write("Input the size of the array: ");
+// int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = CreateArray(sizeArray);
+// ShowArray(array);
+
+
